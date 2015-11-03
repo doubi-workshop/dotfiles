e2809e9b508bebf01031cbb70fa493837192357e
Zhang Kang <zk54188@gmail.com>
2015-11-03 17:54:03 +0800

    add ssh config file and Vundle install to link_config_sh

--- script/link_config.sh
+++ script/link_config.sh
@@ -4,47 +4,54 @@
 echo "soft link all config files"
 
 # personal script
-if ! [ -d "$HOME/bin" ]; then
-    ln -s $DOTFILE_PATH/script $HOME/script
+if ! [ -d "${HOME}/bin" ]; then
+    ln -s ${DOTFILE_PATH}/script ${HOME}/script
 fi
 echo "soft link personal script"
 
 # bash
-if [ -e "$HOME/.bashrc" ]; then
-    rm $HOME/.bashrc
+if [ -e "${HOME}/.bashrc" ]; then
+    rm ${HOME}/.bashrc
 fi
-ln -s $DOTFILE_PATH/bash/bashrc $HOME/.bashrc
-if [ -e "$HOME/.bash_profile" ]; then
-    rm $HOME/.bash_profile
+ln -s ${DOTFILE_PATH}/bash/bashrc ${HOME}/.bashrc
+if [ -e "${HOME}/.bash_profile" ]; then
+    rm ${HOME}/.bash_profile
 fi
-ln -s $DOTFILE_PATH/bash/bash_profile $HOME/.bash_profile
+ln -s ${DOTFILE_PATH}/bash/bash_profile ${HOME}/.bash_profile
 echo "soft link bash"
 
 # vim
-if [ -d "$HOME/.vim" ]; then
-    rm -rf $HOME/.vim
+if [ -d "${HOME}/.vim" ]; then
+    rm -rf ${HOME}/.vim
 fi
-if [ -e "$HOME/.vimrc" ]; then
-    rm $HOME/.vimrc
+if [ -e "${HOME}/.vimrc" ]; then
+    rm ${HOME}/.vimrc
 fi
-ln -s $DOTFILE_PATH/vim/vimrc.vim $HOME/.vimrc
+ln -s ${DOTFILE_PATH}/vim/vimrc.vim ${HOME}/.vimrc
 echo "soft link vim"
 
 # zsh
-#if [ -e "$HOME/.zshrc" ]; then
-#    rm $HOME/.zshrc
+#if [ -e "${HOME}/.zshrc" ]; then
+#    rm ${HOME}/.zshrc
 #fi
-#if [ -d "$HOME/.oh-my-zsh" ]; then
-#    rm -rf $HOME/.oh-my-zsh
+#if [ -d "${HOME}/.oh-my-zsh" ]; then
+#    rm -rf ${HOME}/.oh-my-zsh
 #fi
-#ln -s $DOTFILE_PATH/zsh/templates/zshrc.zsh-template $HOME/.zshrc
-#ln -s $DOTFILE_PATH/zsh/ $HOME/.oh-my-zsh 
+#ln -s ${DOTFILE_PATH}/zsh/templates/zshrc.zsh-template ${HOME}/.zshrc
+#ln -s ${DOTFILE_PATH}/zsh/ ${HOME}/.oh-my-zsh 
 
 # tmux
-if [ -e "$HOME/.tmux.conf" ]; then
-    rm $HOME/.tmux.conf
+if [ -e "${HOME}/.tmux.conf" ]; then
+    rm ${HOME}/.tmux.conf
 fi
-ln -s $DOTFILE_PATH/tmux/tmux.conf $HOME/.tmux.conf
+ln -s ${DOTFILE_PATH}/tmux/tmux.conf ${HOME}/.tmux.conf
 echo "soft link tmux"
+
+# install Vundle for vim
+if ![ -d "${HOME}/.vim/bundle/Vundle.vim" ]; then 
+    git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
+    vim +PluginInstall +qall
+fi
+echo "install Vundle for vim"
 
 echo "done"
