5f15895ba4805ce6cb2591dbf24a28ec7447c5e5
Zhang Kang <zk54188@gmail.com>
2015-11-26 12:17:04 +0800

    chang bashrc c.snippets

--- script/backup_vm_dotfiles.sh
+++ script/backup_vm_dotfiles.sh
@@ -2,4 +2,4 @@
 echo "backup vm files to pc"
 echo ""
 
-rsync -avzh --update -e ssh vm:/home/zhangkang-pd/dotfiles /cygdrive/e/Cloud/
+rsync -avzh --update -e ssh vm:${HOME}/dotfiles /cygdrive/e/Cloud/
